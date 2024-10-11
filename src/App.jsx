c8a3d5b1aeb12922fbedfffb6ca056aa24d7b167
Denys Dashkevych <majorprestige99@gmail.com>
2024-10-11 13:19:15 -0500

    check updated project structure

--- src/App.jsx
+++ src/App.jsx
@@ -44,7 +44,7 @@ function App() {
 
   return (
     <>
-    <h1>Auto deploy check</h1>
+    <h1>Check repo</h1>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
