ed94dd082f4cd946a5c85f1572ff4270064d4abc
Denys Dashkevych <majorprestige99@gmail.com>
2024-10-11 13:45:30 -0500

    added skeleton for project

--- src/App.jsx
+++ src/App.jsx
@@ -1,99 +1,38 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import ProductList from './ProductList';
 import './App.css';
+import AboutUs from './AboutUs';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [showProductList, setShowProductList] = useState(false);
 
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    feedback: '',
-  });
-
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log('submitted')
-    const confirmationMessage = `
-  Name: ${formData.name}
-  Email: ${formData.email}
-  Feedback: ${formData.feedback}
-  `;
-    const isConfirmed = window.confirm(
-      `Please confirm your details:\n\n${confirmationMessage}`
-    );
-    if (isConfirmed) {
-      console.log('Submitting feedback:', formData);
-      setFormData({
-        name: '',
-        email: '',
-        feedback: '',
-      });
-      alert('Thank you for your valuable feedback!');
-    }
+  const handleGetStartedClick = () => {
+    setShowProductList(true);
   };
 
   return (
-    <>
-    <h1>Check repo</h1>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="app-container">
+      <div className={`landing-page ${showProductList ? 'fade-out' : ''}`}>
+        <div className="background-image"></div>
+        <div className="content">
+          <div className="landing_content">
+            <h1>Welcome To Paradise Nursery</h1>
+            <div className="divider"></div>
+            <p>Where Green Meets Serenity</p>
 
-      <form className="feedback-form" onSubmit={handleSubmit}>
-        <h2>We`d Love to Hear From You!</h2>
-        <p>Please share your feedback with us.</p>
-        <input
-          type="text"
-          name="name"
-          placeholder="Your Name"
-          value={formData.name}
-          onChange={handleChange}
-        />
-        <input
-          type="email"
-          name="email"
-          placeholder="Your Email"
-          value={formData.email}
-          onChange={handleChange}
-        />
-        <textarea
-          name="feedback"
-          placeholder="Your Feedback"
-          value={formData.feedback}
-          onChange={handleChange}
-        ></textarea>
-        <button type="submit">
-          Submit Feedback
-        </button>
-      </form>
-    </>
+            <button className="get-started-button" onClick={handleGetStartedClick}>
+              Get Started
+            </button>
+          </div>
+          <div className="aboutus_container">
+            <AboutUs />
+          </div>
+        </div>
+      </div>
+      <div className={`product-list-container ${showProductList ? 'visible' : ''}`}>
+        <ProductList />
+      </div>
+    </div>
   );
 }
 
